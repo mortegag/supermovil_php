2d94a78adc5b54ec8924020a072a9b7629dded0a
Moises Ortega <mortegag@hotmail.com>
2024-01-18 20:05:23 -0500

    inclui ayuda de la activacion

---
+++ ayuda_activar.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Ayuda para recargar o activar servicio de Super Movil</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<br>
+<div><p><center><strong>ACTIVACION DEL SERVICIO SUPER MOVIL CHOFER</strong></center></p></div>
+<br>
+<p>Última actualización:15/03/2023</p>
+
+<p><em>POR FAVOR LEER CUIDADOSAMENTE ESTE DOCUMENTO.</em></p>
+ 
+<p>Para activar los servicios de Super Móvil Chofer usted deberá realizar un prepago con la banca electrónica con YAPPY, 
+   dicho pago le permitirá utilizar los servicios de SUPER MOVIL en todo momento.</p> 
+<p><em>IMPORTANTE..!</em>: Si desea utilizar continuamente los servicios de SUPER MOVIL, mantenga un saldo positivo.</p>
+<br>
+ 
+<p><b>PRIMER PASO : Pagar la cantidad deseada con Yappy</b></p>
+<p>Cuando entre en su banca en línea , vaya a la opción de transaccionar y seleccione Yappy, desde el menú de clic al botón "Enviar" y luego seleccione "Directorio".</p>
+<p>En el "Directorio" puede buscar la empresa "SUPER MOVIL" y estará listo para realizar su prepago con la cantidad  deseada para activar su servicio , 
+al realizar el pago, Yappy se generara un documento virtual que contiene los datos generales de su transacción, guarde ese documento en su celular u ordenador.</p>
+<br> 
+
+<p><b>OBSERVACION : Copiar el código de "Confirmación" del documento de transacción o pago</b></p>
+<p>Tal como se describe en el "primer paso" cada pago o transacción tiene datos generales de la transacción como nombre, fecha, cantidad , confirmación etc.</p>
+<p><em>Importante!!!</em> Guarde, mantenga o copie de la transacción el denominado código de "Confirmación" descrito en el documento de pago, para utilizarlo luego en el App Super Móvil.</p>
+<p>Dicho código es fácil distinguirlo porque consta de LETRAS-NUMEROS y dice "CONFIRMACION".</p>
+
+<br>
+
+<p><b>SEGUNDO PASO : Ingresar el código de "Confirmación" en SUPER MOVIL CHOFER</b></p>
+<p>Ingrese este código en el aplicativo en la opción 3 puntos(...) "Activar servicio" a la derecha superior del app , desde aquí podrá activar el servicio con el código de "Confirmación" de su pago</p>
+<p>Al momento de instalar por primera vez el APP SUPER MOVIL CHOFER , el sistema automáticamente le llevara a la opción "Activar Servicio", dado que es primera vez y necesita ser activado con la "Confirmación" del prepago realizado</p>
+<p>Si usted ya es usuario de SUPER MOVIL CHOFER y se quedo sin saldo, recuerde !!! que no podrá "Conectarse" para brindar servicios a usuarios, debe realizar los pasos "UNO" y "DOS" para activar el servicio.</p>
+<p>Para estar pendiente de su saldo ,puede verificar su estado en cuenta en la opción de 3 puntos(...) a la derecha de la parte superior del APP en "Estado de Cuenta".</p>
+
+<br>
+
+<p><b>PROBLEMAS O ERRORES CON LA ACTIVACION</b></p>
+
+<p>Por favor siga las siguientes recomendaciones :</p>
+<p><b>1. Anote o Escriba la confirmación en SUPER MOVIL, tal como esta escrita en el documento virtual de pago Yappy</b></p>
+<p>-  Ingrese la confirmación sin cometer errores de omisión de alguna letra o numero y escriba el código en mayúscula</p>
+<p>-  Espera el menos 4 minutos, después de realizar el pago con Yappy y luego introduzca su código de confirmación en Super Móvil</p>
+
+<p><b>2. En caso de errores en pasos 1 y 2 y para evitar fraudes de Activación y por la seguridad de nuestros Servicios y de los Usuarios de Super Móvil , se debe:</b></p>
+<p>-  Solo utilizar este forma de activación en caso de haber agotado de forma cuidadosa los pasos 1 y 2 , y no haber podido ser activado en el Sistema Super Movil</p>
+<p>-  Puede enviar un correo electronico a activacion@supermovilapp.com y adjuntar la copia del pago electronico generada por yappy. </p>
+<p>- <am>Importante..!<am/>adjuntar el documento de pago o slip de transacción.
+<p>-  Super Móvil atenderá el reclamo en 24 horas y le contactara  para activar su servicio. </p>
+<p>-  En caso de que usted lo desee, tambien puede solicitar a SUPER MOVIL la devolucion de lo pagado, previa verificacion de su transaccion. </p>
+<p>-  Nos esforzamos por proteger la información personal, los derechos y los intereses legítimos de Super Móvil  y de nuestros Usuarios;</p>
+
+<p><b>3. ¡Garantizado...! El dinero pagado a Super Móvil es debidamente registrado en la banca electrónica:</b></p>
+<p>-  Si usted ha realizado un pago a Super Móvil , esta transacción mediante Yappy se refleja en PANEL DE CONTROL DE YAPPY COMERCIAL de la empresa.</p>
+<p>-  Estos movimientos de transacciones, son la evidencia de su pago para la activación de nuestros servicios</p>
+<p>-  De existir un reclamo de pago , este pago debera reflejarse en esta plataforma y sera validada y confirmada por SUPER MOVIL, para su debida gestión.</p>
+<p>-  Tome en cuenta que SUPER MOVIL ha realizado esfuerzos para facilitar tanto la seguridad del pago como la seguridad en la activación del servicio SUPER MOVIL</p>
+
+<p><b>4. Facilitando al usuario la activación del servicio: </b></p>
+<p>-  La activación del servicio desde la comodidad de la banca electrónica y con el APP en un "SOLO" lugar, evita la incomodidad de uso Kioscos On line y otros medios de activación</p>
+<p>-  En una primera instancia el usuario de Super Movil podra activar por si mismo el servicio, y solo en casos de dificultades tendra a mano la ayuda del punto 2 </p>
+<p>-  Por favor no utilice el punto 2 como una regla de uso, ni nos envie documentacion fraudulenta o engañosa</p>
+<p>-  Esta gestión es parte de nuestros esfuerzos por proporcionarle una buena experiencia al usuario</p>
+<p>-  <am>DESEAMOS Y NOS ESFORZAMOS POR BRINDARLE UN EXCELENTE SERVICIO...!</am>La activación de dos pasos es la forma que le ahorrara tiempo y dinero para seguir activado.</p>
+
+
+</body>
+</html>
